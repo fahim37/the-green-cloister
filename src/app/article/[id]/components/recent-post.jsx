3bfafa9b0114746e4acf20d89fc2ba687ff4a8e5
Fahim <ahmed.fahim37@gmail.com>
2025-02-18 16:32:06 +0600

    minor

--- src/app/article/[id]/components/recent-post.jsx
+++ src/app/article/[id]/components/recent-post.jsx
@@ -29,7 +29,11 @@ const articles = [
 ];
 const RecentPost = () => {
   return (
-    <div>
+    <div className="container">
+      <h1 className="text-4xl font-light text-textPrimary flex items-center justify-center my-[30px] lg:my-[50px]">
+        <div className="h-8 w-2 bg-primary mr-2"></div>
+        Recent Posts
+      </h1>
       <ArticleGrid articles={articles} />
     </div>
   );
